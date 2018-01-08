85769552c504dc5feb39a4b90b7cfd63afabac0e
mesnardo <mesnardo@gwu.edu>
2018-01-08 13:21:47 -0500

    [Colonial] Add input files for 3D snake tests 2k35

---
+++ runs/colonialone/snake3dRe2000AoA35/amgx_classical/5nodes/scripts/createBody.py
@@ -0,0 +1,29 @@
+"""
+Generates the boundary points of a 3D cylinder with a flying-snake
+cross-section at angle-of-attack 35 degrees.
+
+We read the coordinates from the file `snakeAoA35.txt`, which contains the 2D
+coordinates used by Anush with cuIBM.
+
+The cylinder is extruded in the spanwise direction over a distance of 3.2c,
+where c is the chord-length of the cross-section, which is 1.0 here.
+
+We choose a spacing of 0.008c in the x-y plane and a spacing of 0.08c in the
+spanwise direction.
+
+We save the boundary coordinates into the file `flyingSnake3dAoA35.body`.
+"""
+
+import os
+from snake.geometry import Geometry2d
+
+
+if not os.environ.get('AZ_SNAKE'):
+  raise KeyError('Environment variable AZ_SNAKE is not set')
+
+filepath = os.path.join(os.environ['AZ_SNAKE'], 'resources', 'geometries',
+                        'snakeAoA35.txt')
+section = Geometry2d(file_path=filepath)
+section.discretization(ds=0.008)
+cylinder = section.extrusion(limits=(0.0, 3.2), ds=0.08)
+cylinder.write(file_path='flyingSnake3dAoA35.body')
